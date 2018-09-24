1de0fa1e4066158442c627d81ea367f8f914b873
Nghia Huynh <huynhn@augsburg.edu>
2018-09-24 14:08:36 -0500

    Updated code to work with txt file.

--- FileParse.py
+++ FileParse.py
@@ -1,47 +1,70 @@
+classdeptshort = []
+classnum = []
+classtitle = []
+classcredits = []
+classdesc = []
+classcore = []
+classprereq = []
+classdeptlong = []
+
+
 def main():
-    file = open("Document.txt", "r")
-    while True:
-        line = file.readline()
-        if " - " in line:
-            departmentname = line
-            print(departmentname)
-            deptshort = departmentname[:3]
-            getcoursenumber(deptshort)
-        if not line:
-            break
-    file.close()
+    getclassdeptandnum()
+    getclasstitleandcredits()
+    getclassdescandcoreandprereq()
+    getclassdeptlong()
 
 
-def getcoursenumber(dept):
-    file = open("Document.txt")
-    while True:
-        line2 = file.readline()
-        if dept in line2 and " - " not in line2 and len(line2) == 7:
-            print(line2)
-            CourseName = file.readline()
-            print(CourseName)
-            description = ""
-            check = True
-            while check:
-                line3 = file.readline()
-                if "Core Curriculum Component" in line3:
-                    Core = line3
-                    check = False
-                else:
-                    description += line3
-            print(description)
 
-            check2 = True
-            while check2:
-                line4 = file.readline()
-                if "Prerequisite(s):" in line4:
-                    Prereq = line4
-                    check2 = False
-            print(Core)
-            print(Prereq)
-        if not line2:
-            break
+def getclassdeptandnum():
+        file = open("Document.txt")
+        while True:
+            line1 = file.readline()
+            if any(str.isdigit(c) for c in (line1[-4:])) and any(str.isalpha(l) for l in (line1[:3])) and len(line1) == 7:
+                classdeptshort.append(line1[:3])
+                classnum.append(line1[-4:])
+            if not line1:
+                break
+
+
+def getclasstitleandcredits():
+        for x, dept in enumerate(classdeptshort):
+            classdeptnum = classdeptshort[x] + classnum[x]
+            with open("Document.txt") as file:
+                for num, line in enumerate(file):
+                    if classdeptnum in line:
+                        lines = open("Document.txt").readlines()
+                        classtitle.append(lines[num+1])
+                        classcredits.append(lines[num+2])
+
+
+def getclassdescandcoreandprereq():
+    for x, dept in enumerate(classdeptshort):
+        classdeptnum = classdeptshort[x] + classnum[x]
+        with open("Document.txt") as file:
+            for num, line in enumerate(file):
+                if classdeptnum in line:
+                    lines = open("Document.txt").readlines()
+                    i = num + 3
+                    desc = ""
+                    while "Core Curriculum Component:" not in lines[i]:
+                        if len(lines[i]) == 2:
+                            desc += ""
+                            i += 1
+                        else:
+                            desc += lines[i]
+                            i += 1
+                    classdesc.append(desc)
+                    classcore.append(lines[i])
+                    classprereq.append(lines[i+1])
+
+def getclassdeptlong():
+    for x, dept in enumerate(classdeptshort):
+        key = classdeptshort[x] + " "
+        with open("Document.txt") as file:
+            for num, line in enumerate(file):
+                if key in line and " " in line[5:6] and " " in line[3:4]:
+                    classdeptlong.append(line)
 
 
 main()
-
